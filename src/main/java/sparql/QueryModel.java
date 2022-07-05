9df4cf7d1f1b4e05b47d25992e5fa663ca04a102
vcth4nh <tvu25337@gmail.com>
2022-07-05 14:10:11 +0700

    Add SelectModel class: - Query data from a Model object
    Add QueryModel class:
    - Contain prefix for Construct and Select queries
    
    Rename GetModel to ConstructModelOnline
    
    Add Main class to test the program

---
+++ src/main/java/sparql/QueryModel.java
@@ -0,0 +1,28 @@
+package sparql;
+
+import org.apache.jena.sparql.core.Prologue;
+
+import java.util.HashMap;
+
+public class QueryModel {
+    protected final Prologue urlPrefix = new Prologue();
+    protected final HashMap<String, String> abbrPrefix = new HashMap<>();
+
+    public QueryModel() {
+        urlPrefix.setPrefix("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+        urlPrefix.setPrefix("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+        urlPrefix.setPrefix("dbr", "http://dbpedia.org/resource/");
+        urlPrefix.setPrefix("dbo", "http://dbpedia.org/ontology/");
+        urlPrefix.setPrefix("dbp", "http://dbpedia.org/property/");
+        urlPrefix.setPrefix("geo", "http://www.w3.org/2003/01/geo/wgs84_pos#");
+        urlPrefix.setPrefix("gold", "http://purl.org/linguistics/gold/");
+        urlPrefix.setPrefix("yago", "http://dbpedia.org/class/yago/");
+
+        abbrPrefix.put("label", "rdfs");
+        abbrPrefix.put("abstract", "dbo");
+        abbrPrefix.put("type", "rdf");
+        abbrPrefix.put("lat", "geo");
+        abbrPrefix.put("long", "geo");
+        abbrPrefix.put("hypernym", "gold");
+    }
+}
