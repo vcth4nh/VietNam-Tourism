d6412f5fd0c6ba330433608d275ac4b36c2228c5
vcth4nh <tvu25337@gmail.com>
2022-07-06 00:38:13 +0700

    Improve getQueryAttr()

--- src/main/java/utils/GetClassInfo.java
+++ src/main/java/utils/GetClassInfo.java
@@ -1,22 +1,59 @@
 package utils;
 
 import tourismobject.TourismObject;
+import tourismobject.park.NationalPark;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.lang.reflect.Modifier;
+
+import static org.junit.Assert.assertTrue;
 
 public class GetClassInfo {
-    public static ArrayList<String> getInheritedFieldsName(Object obj) {
+
+    /**
+     * Return an ArrayList of fields' name
+     */
+    public static ArrayList<String> getQueryAttr(Object obj) {
+        ArrayList<String> queryAttr = new ArrayList<>();
         Class<?> class_ = obj.getClass();
-        ArrayList<String> allFieldsName = new ArrayList<>();
+        boolean haveLabel = false, haveAbstract = false;
 
         while (class_ != TourismObject.class.getSuperclass()) {
             Field[] allFields = class_.getDeclaredFields();
             for (Field field : allFields) {
-                allFieldsName.add(field.getName());
+                if (Modifier.isStatic(field.getModifiers())) continue;
+
+                String fieldName = field.getName();
+
+                if (fieldName.charAt(fieldName.length() - 1) == '_')
+                    fieldName = rmLastChar(fieldName);
+
+                if (fieldName.equals("label")) haveLabel = true;
+                if (fieldName.equals("abstract")) haveAbstract = true;
+
+                queryAttr.add(fieldName);
             }
+
             class_ = class_.getSuperclass();
         }
-        return allFieldsName;
+        if (!haveLabel) {
+            throw new RuntimeException(class_ + " doesn't have label field");
+        }
+
+        if (!haveAbstract) {
+            throw new RuntimeException(class_ + " doesn't have abstract field");
+        }
+
+        return queryAttr;
+    }
+
+    public static String rmLastChar(String s) {
+        return s.substring(0, s.length() - 1);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> queryAttr = GetClassInfo.getQueryAttr(new NationalPark());
+        System.out.println(queryAttr);
     }
 }
