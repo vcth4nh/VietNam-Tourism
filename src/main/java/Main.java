58263e2b7cf43003402d9a8e5f5372a58daf3d1c
vcth4nh <tvu25337@gmail.com>
2022-07-07 14:43:24 +0700

    Minor fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,5 @@
-import org.apache.jena.rdf.model.Model;
-import sparql.ConstructModelOnline;
-
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 public class Main {
-    public static void main(String[] args) {
-        ConstructModelOnline cmo = new ConstructModelOnline();
-        Model m = cmo.ExecConstruct();
-        assertNotNull("Failed to query data", m);
-    }
+
 }
