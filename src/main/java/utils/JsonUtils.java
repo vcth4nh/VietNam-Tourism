c5aec4ffdaa4559ff2cb1ed2b2f529c962b9bc1a
vcth4nh <tvu25337@gmail.com>
2022-07-08 02:04:26 +0700

    New

---
+++ src/main/java/utils/JsonUtils.java
@@ -0,0 +1,19 @@
+package utils;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class JsonUtils {
+    public static JSONObject read(String fileName) throws IOException, ParseException {
+        return (JSONObject) new JSONParser().parse(new FileReader(fileName));
+    }
+
+    public static ArrayList<String> getJsonKeys(JSONObject json) throws ClassCastException {
+        return new ArrayList<String>(json.keySet());
+    }
+}
