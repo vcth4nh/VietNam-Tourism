6da8f9476fb24e43d5033e9f9ca82a29ae11d772
vcth4nh <tvu25337@gmail.com>
2022-07-07 14:43:00 +0700

    Minor fix

--- src/main/java/sparql/QueryModel.java
+++ src/main/java/sparql/QueryModel.java
@@ -1,5 +1,7 @@
 package sparql;
 
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.sparql.core.Prologue;
 
 import java.util.HashMap;
@@ -7,7 +9,6 @@ import java.util.HashMap;
 public class QueryModel {
     protected final Prologue urlPrefix = new Prologue();
     protected final HashMap<String, String> abbrPrefix = new HashMap<>();
-    protected final HashMap<String, String> modelCriteria = new HashMap<>();
 
     public QueryModel() {
         urlPrefix.setPrefix("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
@@ -25,15 +26,15 @@ public class QueryModel {
         abbrPrefix.put("lat", "geo");
         abbrPrefix.put("long", "geo");
         abbrPrefix.put("hypernym", "gold");
+        abbrPrefix.put("testttttt", "gold");
+
     }
 
-    public String getPredicate(String s) {
+    protected String getPredicate(String s) {
         return abbrPrefix.get(s) + ':' + s;
     }
 
-    public String getSubject(String s) {
+    protected String getSubject(String s) {
         return "?" + s;
     }
-
-
 }
