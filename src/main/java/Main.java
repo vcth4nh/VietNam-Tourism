3db0cf11655d43e28af2e346867cb5bea374558b
vcth4nh <tvu25337@gmail.com>
2022-07-08 02:05:12 +0700

    Test program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,50 @@
-import static org.junit.Assert.assertNotNull;
+import org.apache.jena.rdf.model.Model;
+import sparql.Construct;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Construct queryOnline = new Construct();
+        Model db = queryOnline.queryOnlineAll();
+        assertNotEquals("Empty database", 0, db.size());
+
+        API4GUI api4GUI = new API4GUI();
+
+
+//      Test API
+        System.out.println("TourismObject: ");
+        System.out.println(api4GUI.getDirectSubclassesName("TourismObject"));
+        System.out.println(api4GUI.ObjectToJson("TourismObject"));
+        try {
+            System.out.println("Press enter");
+            System.in.read();
+            System.in.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Building: ");
+        System.out.println(api4GUI.getDirectSubclassesName("Building"));
+        System.out.println(api4GUI.ObjectToJson("Building"));
+        try {
+            System.out.println("Press enter");
+            System.in.read();
+            System.in.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Park: ");
+        System.out.println(api4GUI.getDirectSubclassesName("Park"));
+        System.out.println(api4GUI.ObjectToJson("Park"));
+
+        api4GUI.destroyCache();
+    }
+
+
+//        ----------------------------------
+//        GUI code goes here
+//        ----------------------------------
 
 }
